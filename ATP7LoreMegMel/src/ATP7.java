f0cb229eb9b018babb85d21cc09abe13e0bef080
lorenascimentoo <lorenanascimento28@gmail.com>
2021-09-20 15:52:54 -0300

    Pratica 7 - atualizacao - numeros primos

--- ATP7LoreMegMel/src/ATP7.java
+++ ATP7LoreMegMel/src/ATP7.java
@@ -13,9 +13,9 @@ public class ATP7 {
                 System.out.println("--------------Cálculo de Números Pares-----------");
                 System.out.println("Informe um número: ");
                 int numero = sc.nextInt();
-                for (int i = 0; i <= numero ; i++) {
+                for (int i = 0; i <= numero; i++) {
 
-                    if ( i % 2 == 0) {
+                    if (i % 2 == 0) {
                         System.out.println(i);
                     }
                 }
@@ -24,27 +24,53 @@ public class ATP7 {
                 System.out.println("---------------Cálculo de Números Ìmpares----------------");
                 System.out.println("Informe um número: ");
                 int numero2 = sc.nextInt();
-                for (int i = 0; i <= numero2 ; i++) {
+                for (int i = 0; i <= numero2; i++) {
 
-                    if ( i % 2 != 0) {
+                    if (i % 2 != 0) {
                         System.out.println(i);
                     }
                 }
                 break;
             case 3:
-                for (int i = 0; i < 150; i++) {
-                    boolean primo = true;
-                    for (int j = 2; j < i; j++) {
-                        if (i % j == 0) {
-                            primo = false;
-                        }
+                System.out.println("---------------Cálculo de Números Ìmpares----------------");
+                System.out.println("Informe um número: ");
+                int numero3 = sc.nextInt();
 
-                    }
-                    if (primo) {
-                        System.out.println(i);
+                for (int i = 0; i <= numero3; i++) {
+                    //inicia validacao do numero primo
+                    boolean valido = true;
+                    while (valido) {
+                        //numeros menores que dois não são primos
+                        if (i < 2) {
+                            valido = false;
+                            //o dois é o primeiro numero primo
+                        } else if (i == 2) {
+                            System.out.println(i);
+                            valido = false;
+                        } else {
+                            //variavel que indica o numero primo e que permite a impressao do numero em tela
+                            boolean primo = true;
+                            //varredura de todos os numeros a partir de 2 ate o numero anterior ao numero testado
+                            for (int cont = 2; cont < i; cont++) {
+                                // se o numero for divisivel por qualquer numero, ele deixa de ser primo
+                                // nega a variavel primo(que permite a impressao na tela, sai do laco for
+                                // e nega a validade do numero quebrando o laco de validacao do numero
+                                if (i % cont == 0) {
+                                    valido = false;
+                                    primo = false;
+                                    break;
+                                }
+                            }
+
+                            //se apos a leitura do laco 'for' nao for encontrado nenhum divisor para o numero, entao ele e primo
+                            if (primo) {
+                                System.out.println(i);
+                                valido = false;
+                            }
+                        }
                     }
                 }
-
+                break;
         }
     }
 }
